186b0870e8387a2a14b1809923a0cb82131be625
Andrea O <17aolav@gmail.com>
2023-06-29 22:27:47 -0400

    Initial Commit

---
+++ KWKFINALPROJ-EXTRA/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  KWKFINALPROJ-EXTRA
+//
+//  Created by Andrea Olavarrieta on 6/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack  {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("Hello, world!")
+
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KWKFINALPROJ-EXTRA/KWKFINALPROJ_EXTRAApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKFINALPROJ_EXTRAApp.swift
+//  KWKFINALPROJ-EXTRA
+//
+//  Created by Andrea Olavarrieta on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct KWKFINALPROJ_EXTRAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
